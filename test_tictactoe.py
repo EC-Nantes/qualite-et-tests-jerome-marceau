02c4ef3deb744797246f9bed80666c7d84e549e9
Walter-D-White <marceau.lafargue@gmail.com>
2023-12-08 15:37:28 +0100

    good tests

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,46 +1,40 @@
 import pytest
 import tictactoe
 
-def test_board():
-    board = tictactoe.Board()
-    assert board.check_win() is False
-    assert board.new_move(0, 0) is True
-    assert board.new_move(0, 0) is False
-    assert board.check_win() is False
-    assert board.new_move(0, 1) is True
-    assert board.new_move(0, 2) is True
-    assert board.check_win() == "x"
-    assert board.new_move(1, 1) is True
-    assert board.new_move(2, 2) is True
-    assert board.check_win() == "x"
-    assert board.new_move(1, 0) is True
-    assert board.new_move(2, 0) is True
-    assert board.check_win() == "x"
-    assert board.new_move(1, 2) is True
-    assert board.check_win() == "x"
-    assert board.new_move(2, 1) is True
-    assert board.check_win() == "x"
-    assert board.new_move(2, 2) is False
-    assert board.check_win() == "x"
-    assert board.new_move(1, 1) is False
-    assert board.check_win() == "x"
-    assert board.new_move(0, 0) is False
-    assert board.check_win() == "x"
-    assert board.new_move(0, 1) is False
-    assert board.check_win() == "x"
-    assert board.new_move(0, 2) is False
-    assert board.check_win() == "x"
-    assert board.new_move(1, 0) is False
-    assert board.check_win() == "x"
-    assert board.new_move(2, 0) is False
-    assert board.check_win() == "x"
-    assert board.new_move(1, 2) is False
-    assert board.check_win() == "x"
-    assert board.new_move(2, 1) is False
-    assert board.check_win() == "x"
-    assert board.new_move(2, 2) is False
-    assert board.check_win() == "x"
-    assert board.new_move(1, 1) is False
-    assert board.check_win() == "x"
-    assert board.new_move(0, 0) is False
-    assert board.check_win() == "x"
+def board1():
+    board1 = tictactoe.Board()
+    board1.grid = [['o', 'x', 'o'], ['o', 'o', 'x'], ['o', 'x', 'o']]
+    return board1
+
+def board2():
+    board2 = tictactoe.Board()
+    board2.grid = [['x', 'x', 'o'], ['x', 'x', 'x'], ['o', 'o', 'x']]
+    return board2
+
+def board3():
+    board3 = tictactoe.Board()
+    board3.grid = [['x', 'o', 'o'], ['x', 'o', 'o'], ['o', 'x', 'x']]
+    return board3
+
+def board4():
+    board4 = tictactoe.Board()
+    board4.grid = [['x', ' ', 'o'], [' ', 'x', 'o'], [' ', 'o', 'x']]
+    return board4
+
+def test_check_column():
+    assert board1().check_column(0) == 'o'
+    assert board2().check_column(1) == False
+    assert board3().check_column(2) == False
+
+def test_check_line():
+    assert board1().check_line(0) == False
+    assert board2().check_line(1) == 'x'
+    assert board3().check_line(2) == False
+
+def test_check_diagonal():
+    assert board1().check_diagonal(0) == 'o'
+    assert board2().check_diagonal(0) == 'x'
+    assert board3().check_diagonal(1) == 'o'
+
+def test_check_win():
+    assert board4().check_win() == 'x'
